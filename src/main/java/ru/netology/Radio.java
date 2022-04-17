e109ac84b3a9d34ab0ad4b918e856938dda0d0e4
Ekaterina <3022805283@qq.com>
2022-04-17 18:02:31 +0800

    version2 updated

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,13 +1,22 @@
 package ru.netology;
 
 public class Radio {
-    private int currentVolume;
-    private int currentChannel;
+    private int currentVolume = 5;
+    private int currentChannel = 0;
+
+    public int getCurrentVolume() {return currentVolume;}
+    public int getCurrentChannel() {return currentChannel;}
 
     public void setCurrentVolume(int newCurrentVolume){
+        if (newCurrentVolume > 10 || newCurrentVolume < 0){
+            return;
+        }
         currentVolume = newCurrentVolume;
     }
     public void setCurrentChannel(int newCurrentChannel){
+        if (newCurrentChannel > 9 || newCurrentChannel < 0){
+            return;
+        }
         currentChannel = newCurrentChannel;
     }
     public int increaseVolume() {
